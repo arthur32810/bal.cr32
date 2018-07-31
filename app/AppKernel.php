f0d608d83b4d8d64352076b7714afd0ac6b347ee
arthur robert <arthur32810@gmail.com>
2018-07-31 14:45:10 +0200

    création du bundle questionnaire

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new CR32\QuestionnaireBundle\CR32QuestionnaireBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/CR32/QuestionnaireBundle/CR32QuestionnaireBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CR32\QuestionnaireBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CR32QuestionnaireBundle extends Bundle
+{
+}
